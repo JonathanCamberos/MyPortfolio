99799215a3385e55a11cd65570d604bb0de7e1f5
JonathanCamberos <jonathancamberos@gmail.com>
2024-07-27 19:20:52 -0600

    starter code

---
+++ holder/src/app/page.js
@@ -0,0 +1,12 @@
+
+
+// Documentation:
+// the page.js the home 'router' --> http://localhost:3000/
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+        hello world!
+    </main>
+  );
+}
