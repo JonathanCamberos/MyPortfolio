30132c853c608d6100577928b39333fc807cb3de
JonathanCamberos <jonathancamberos@gmail.com>
2024-07-27 21:13:11 -0600

    formating header icon

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -5,11 +5,11 @@ import profileImg from "@/public/profile-img.png"
 
 const Logo = () => {
   return (
-    <Link href="/">
+    <Link href="/" className="flex items-center text-dark">
         <div className="w-16 rounded-full overflow border border-solid border-dark">
              <Image src={profileImg} alt="JC-Alt" className="w-full h-auto rounded-full" />
         </div>
-        <span>JC</span>
+        <span className="font-bold text-xl">JC</span>
     </Link>
   )
 }
