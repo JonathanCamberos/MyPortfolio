9caccafd085b3af54813bc6809dc4b68effd8279
JonathanCamberos <jonathancamberos@gmail.com>
2024-07-28 07:47:38 -0600

    replacing @ with direct import statements

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import profileImg from "@/public/profile-img.png"
+import profileImg from "./../../../public/profile-img.png"
 
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark">
         <div className="w-16 rounded-full overflow border border-solid border-dark">
-             {/* <Image src={profileImg} alt="JC-Alt" className="w-full h-auto rounded-full" /> */}
+             <Image src={profileImg} alt="JC-Alt" className="w-full h-auto rounded-full" />
         </div>
         <span className="font-bold text-xl">JC</span>
     </Link>
