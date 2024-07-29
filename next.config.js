3182efc93340892d1445f9c716cea00cf2ccad9a
JonathanCamberos <jonathancamberos@gmail.com>
2024-07-29 11:03:11 -0600

    editde next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+
+const {withContentlayer} = require("next-contentlayer")
+
+const nextConfig = {
+    compiler:{
+        removeConsole: true,
+    }
+};
+
+module.exports = withContentlayer({ ...nextConfig });
