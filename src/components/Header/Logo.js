46811d45ceebadbd917330b0f1daf71b88df8cc8
JonathanCamberos <jonathancamberos@gmail.com>
2024-07-30 13:53:40 -0600

    added fancy line to header

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -6,13 +6,10 @@ import profileImg from "../../../public/profile-img.png"
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark">
-        <div className="w-16 rounded-full overflow border border-solid border-dark mr-4">
-             <Image 
-                src={profileImg} 
-                alt="JC-Alt" 
-                className="w-full h-auto rounded-full" />
+        <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
+            {/* <Image src={profileImg} alt="CodeBucks logo" className="w-full h-auto rounded-full" sizes="20vw" priority /> */}
         </div>
-        <span className="font-bold text-xl">JC</span>
+        <span className="font-bold dark:font-semibold text-lg md:text-xl">JC</span>
     </Link>
   )
 }
