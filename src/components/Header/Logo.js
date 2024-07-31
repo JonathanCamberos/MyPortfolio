79841a1bac93bd9694242fe833fb93655feb36a7
JonathanCamberos <jonathancamberos@gmail.com>
2024-07-31 17:19:50 -0600

    updated header logo + sizing

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import profileImg from "../../../public/profile-img.png"
+import profileImg from "../../../public/icons8-logo-100.png"
 
 const Logo = () => {
   return (
@@ -13,10 +13,13 @@ const Logo = () => {
     
     */
     <Link href="/" className="flex items-center text-dark dark:text-light">
-        <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
-            {/* <Image src={profileImg} alt="CodeBucks logo" className="w-full h-auto rounded-full" sizes="20vw" priority /> */}
+        {/* <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4"> */}
+        <div className=" w-12 md:w-16 overflow-hidden">
+            <Image src={profileImg} alt="CodeBucks logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
         </div>
-        <span className="font-bold dark:font-semibold text-lg md:text-xl">JC</span>
+        <span className="font-bold dark:font-semibold text-lg md:text-xl">
+          jonathancamberos :')
+        </span>
     </Link>
   )
 }
