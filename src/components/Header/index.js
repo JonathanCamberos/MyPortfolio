1caa0717f269fd8b025c6aeed6f985826239a2b9
JonathanCamberos <jonathancamberos@gmail.com>
2024-07-31 17:07:03 -0600

    finish commenting header/index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -40,7 +40,7 @@ const Header = () => {
         <Logo />
 
         {/* 
-            w-max        : | Sizing  : Width   | -> [width: max-content]
+            w-max        : | Sizing  : Width   | -> [width: max-content] w-px, w-1, and w-64 set an element to a fixed width.
             py-3         : | Spacing : Padding | ->
             px-8         : | Spacing : Padding | -> 
             border       : | Borders : Border Width | -> [border-width: 1px] control width of element's borders
@@ -64,12 +64,15 @@ const Header = () => {
             top-6        : | Layout : Top/Right/Bottom/Left | -> [top: 1.5rem; /* 24px]  control placement of positioned elements
                                                                 top-*, right-*, bottom-*, left-*, and inset-* utilities to set the 
                                                                 horizontal or vertical position of a positioned element.
-            right-1/2    :
+            
+            right-1/2    : | Layout : Top/Right/Bottom/Left | -> [right: 50%] 
 
-            translate-x-1/2  :
+            translate-x-1/2  : | Transforms : Translate | -> [transform: translateX(50%)] translate-x-* and translate-y-* utilities to translate an element
             bg-light/80      :
-            backdrop-blur-sm :
-            z-50             :
+            backdrop-blur-sm : | Filters : Backdrop Blur | -> [backdrop-filter: blur(4px)]  applying backdrop blur filters to an element
+            
+            z-50             : | Layout : Z-Index | -> [z-index: 50] control the stack order of an element
+                                                        useful for layering say 5 circles in a certain order
         */}
         <nav className=" w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize items-center hidden sm:flex
         fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50">
@@ -81,9 +84,30 @@ const Header = () => {
             </button>
         </nav>
 
+        
         <div>
-            {/* hover: allows for resize ui feautre*/}
+            {/* 
+              inline-block: | Layout : Display | -> [display: inline-block] 
+                                                    inline, inline-block, and block utilities to control the flow of text and elements.
+              
+              w-6: | Sizing : Width | -> [width: 1.5rem; 24px] w-px, w-1, and w-64 to set an element to a fixed width
+              h-6: | Sizing : Height| -> [height: 1.5rem; 24px] h-px, h-1, and h-64 to set an element to a fixed height
+
+              mr-4: | Spacing : Margin | -> [margin-right: 1rem; 16px]  mt-*, mr-*, mb-*, and ml-* utilities to control the margin on ONE SIDE of an element.
+                                                                        oppose to padding which is default on all sides
+            */}
             <a href="http://" className="inline-block w-6 h-6 mr-4">
+
+              {/*
+                hover:scale-125: | Transforms : Scale | ->   only apply the scale-125 utility on hover.
+                transition-all:  | Transitions & Animation : Transition Property | -> transition-property: all;
+                                                                                      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+                                                                                      transition-duration: 150ms;
+                
+                ease: | Transitions & Animation : Transition Timing Function | ->
+                duration-200: | Transitions & Animation : Transition Duration | -> [transition-duration: 200ms] 
+                
+              */} 
               <LinkedInIcon className="hover:scale-125 transition-all ease duration-200"/>
             </a>
             <a href="http://" className="inline-block w-6 h-6 mr-4">
