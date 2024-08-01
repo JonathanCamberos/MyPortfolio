ccf241f52d09145913db08a5fd701ca4059fdb85
JonathanCamberos <jonathancamberos@gmail.com>
2024-07-31 18:38:35 -0600

    i apologize for the amount of comments

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -7,17 +7,23 @@ import profileImg from "../../../public/icons8-logo-100.png"
 const Logo = () => {
   return (
 
-    /* flex:              :
-       items-center       :
+    /* flex:              | Layout : Display | -> utility to create a block-level flex container
+       items-center:      | Flexbox & Grid : Align Items | -> (used w/ flex) align items along the center of the container’s cross axis (top to bottom)
+
        text-dark          :
        dark:text-light    :
     
     */
     <Link href="/" className="flex items-center text-dark dark:text-light">
-        {/* <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4"> */}
+
+        {/*  */}
         <div className=" w-12 md:w-12 overflow-hidden">
+            
+            {/*  */}
             <Image src={profileImg} alt="CodeBucks logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
         </div>
+
+        {/*  */}
         <span className="font-bold dark:font-semibold text-lg md:text-xl">
           jonathancamberos :')
         </span>
