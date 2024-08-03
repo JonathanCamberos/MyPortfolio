555a63df74c7edb54f8daf95fd9d1701ff69f1d1
JonathanCamberos <jonathancamberos@gmail.com>
2024-08-02 20:21:18 -0600

    starting SEO section

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,17 @@
+const siteMetaData = {
+    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    author: 'jonathancamberos',
+    headerTitle: 'Next.js Blog',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: '', /* update w/ final url */
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: '', 
+    github: 'https://github.com/codebucks27',
+    linkedin: 'https://www.linkedin.com/in/jonathancamberos/',
+    locale: 'en-US'
+  }
+  
+  module.exports = siteMetadata
