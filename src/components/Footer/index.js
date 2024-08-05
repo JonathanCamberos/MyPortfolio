d066313d51766a0ac41511fff6ea4eead3659457
JonathanCamberos <jonathancamberos@gmail.com>
2024-08-04 18:55:09 -0600

    fixed: most pages reponsive/darkmode functional

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -18,35 +18,33 @@ const Footer = () => {
     /* ##### footer component #####
     
     */
-    <footer className="mt-16 rounded-2xl bg-dark m-10 flex flex-col items-center text-light">
+    <footer className="mt-16 rounded-2xl bg-dark m-10 flex flex-col items-center text-light">    
 
         {/* ##### The Foots Foot ##### */}
-        <div className="w-full  mt-16 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex  flex-col md:flex-row items-center justify-between">
-        
-        <span className="text-center">
+        <div className="w-full  relative font-medium border-t border-solid border-light py-6 px-8 flex  flex-col md:flex-row items-center justify-between">
           
-          {/* ##### Footer Icons Div ##### */}
-          <div className="flex items-center">
-              JC :)
+          <span className="text-center">
+            <div className="flex items-center">
+                JC :)
+            </div>
+          </span>
+
+          <Link href="/sitemap.xml" className="text-center underline ml:-4  my-4 md:my-0">
+            sitemap.xml
+          </Link>
+           Thanks for visiting!
+
+          <div>
+              <a href={siteMetadata.linkedin} className="inline-block w-6 h-6 mr-4 ml-5">
+                <LinkedInIcon className="hover:scale-125 transition-all ease duration-200"/>
+              </a>
+
+              <a href={siteMetadata.github} className="inline-block w-6 h-6 mr-4 fill-light">
+                <GithubIcon className="hover:scale-125 transition-all ease duration-200 "/>
+              </a>
           </div>
-        </span>
 
-        <Link href="/sitemap.xml" className="text-center underline ml:-4  my-4 md:my-0">
-          sitemap.xml
-        </Link>
-        
-
-        <div>
-            <a href={siteMetadata.linkedin} className="inline-block w-6 h-6 mr-4 ml-5">
-              <LinkedInIcon className="hover:scale-125 transition-all ease duration-200"/>
-            </a>
-
-            <a href={siteMetadata.github} className="inline-block w-6 h-6 mr-4 fill-light">
-              <GithubIcon className="hover:scale-125 transition-all ease duration-200 "/>
-            </a>
-          
         </div>
-      </div>
         
     </footer>
   )
