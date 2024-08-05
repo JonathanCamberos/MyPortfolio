2fba822a026726f07c391e364b267d58e8c02fab
JonathanCamberos <jonathancamberos@gmail.com>
2024-08-04 19:42:26 -0600

    save: begin adding view counter (might break everything :)  )

--- src/components/Blog/BlogDetails.js
+++ src/components/Blog/BlogDetails.js
@@ -3,6 +3,8 @@ import { slug } from 'github-slugger'
 import Link from 'next/link'
 import React from 'react'
 
+// working
+
 const BlogDetails = ({blog, slug: blogSlug}) => {
   return (
     <div className="px-2  md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5  md:mx-10 rounded-lg">
