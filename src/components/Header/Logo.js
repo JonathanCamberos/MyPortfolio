5ed11a8e972818d7aa13f20e181e09ebba16a2e7
JonathanCamberos <jonathancamberos@gmail.com>
2024-08-08 14:26:16 -0600

    new cat logo

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,7 +1,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import profileImg from "../../../public/pear.png"
+import profileImg from "../../../public/logo/pear.png"
+import cat30 from "../../../public/logo/cat30.png"
+import cat60 from "../../../public/logo/cat60.png"
+import cat90 from "../../../public/logo/cat90.png"
+
 // icon from https://icons8.com/icon/set/react-logo/group-outlined :)
 
 const Logo = () => {
@@ -14,7 +18,7 @@ const Logo = () => {
         <div className="w-8 md:w-8 rounded-full overflow-hidden mr-0">
             
             {/*  ##### Image #####*/}
-            <Image src={profileImg} alt="Jc-alt logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
+            <Image src={cat60} alt="Jc-alt logo" className="w-auto h-auto" sizes="20vw" priority />
         </div>
 
         {/*  ##### Span Div for name #####*/}
