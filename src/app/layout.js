857f828f60d1f1c989514bcc97951e1743ed7b8b
JonathanCamberos <jonathancamberos@gmail.com>
2024-08-14 16:54:37 -0600

    post: leetcode - arrays cleaning

--- src/app/layout.js
+++ src/app/layout.js
@@ -78,11 +78,9 @@ export default function RootLayout({ children }) {
         {/* from tailwindcss.com/docs/dark-mode */}
         <Script id="theme-switcher" strategy="beforeInteractive">
           {`if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            // document.documentElement.classList.add('dark')
-            document.documentElement.classList.remove('dark')
-          } else {
-            // document.documentElement.classList.remove('dark')
             document.documentElement.classList.add('dark')
+          } else {
+            document.documentElement.classList.remove('dark')
           }`}
         </Script>
 
--- src/components/Blog/BlogToc.js
+++ src/components/Blog/BlogToc.js
@@ -3,11 +3,15 @@ import React from 'react'
 const BlogToc = ({ blog }) => {
   return (
     <div className="col-span-12 lg:col-span-3">
+      
+      {/* Open Close TOC */}
       <details
         className="border-[1px] border-solid border-dark dark:border-light text-dark
         dark:text-light rounded-lg p-4 sticky top-6 max-h-[80vh] overflow-hidden overflow-y-auto"
         open
       >
+
+        {/* ##### Table of Contents String ##### */}
         <summary
           className="text-lg font-semibold capitalize cursor-pointer"
           style={{ fontSize: '1.25rem' }}  // Larger font size for overarching TOC summary
@@ -15,11 +19,16 @@ const BlogToc = ({ blog }) => {
           Table Of Content
         </summary>
 
+        {/* ##### List of inner ##### */}
         <ul className="mt-4 font-in text-base">
+
           {/* Iterate over each level 2 heading */}
           {Object.entries(blog.toc).map(([headingText, { slug, subheadings }], index) => (
+
             <li key={`#${slug}`} className="mb-2">
               <details>
+
+                {/* ##### Each Heading 2 ##### */}
                 <summary
                   className="cursor-pointer hover:underline"
                 >
@@ -28,8 +37,10 @@ const BlogToc = ({ blog }) => {
                   </a>
                 </summary>
 
+                {/* ##### Heading 3 List ##### */}
                 <ul className="ml-4 mt-2">
-                  {/* Iterate over each level 3 subheading under the current level 2 heading */}
+                  
+                  {/* ##### Iterate over each level 3 subheading under the current level 2 heading ##### */}
                   {subheadings.map(({ text, slug }) => (
                     <li key={`#${slug}`} className="py-1">
                       <a
@@ -37,9 +48,12 @@ const BlogToc = ({ blog }) => {
                         data-level="three"
                         className="pl-4 sm:pl-6 flex items-center justify-start"
                       >
+                        {/* ##### Dot Thing ##### */}
                         <span className="flex w-1 h-1 rounded-full bg-dark mr-2">
                           &nbsp;
                         </span>
+
+                        {/* ##### Heading 3 Item ##### */}
                         <span className="hover:underline">{text}</span>
                       </a>
                     </li>
@@ -47,12 +61,16 @@ const BlogToc = ({ blog }) => {
                 </ul>
               </details>
 
-              {/* Add horizontal divider after each level 2 heading, except the last one */}
+              {/* ##### Horizontal divider after each level 2 heading, except the last one ##### */}
               {index < Object.entries(blog.toc).length - 1 && (
                 <hr className="border-t border-solid border-dark/20 my-4" />
               )}
             </li>
+
+
           ))}
+
+
         </ul>
       </details>
     </div>
