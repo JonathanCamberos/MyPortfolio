b5749f14ffce1748d2654e7d98d6635d9e64aa85
JonathanCamberos <jonathancamberos@gmail.com>
2024-08-14 17:04:35 -0600

    styling to TOC

--- src/components/Blog/BlogToc.js
+++ src/components/Blog/BlogToc.js
@@ -1,34 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const BlogToc = ({ blog }) => {
   return (
     <div className="col-span-12 lg:col-span-3">
-      
-      {/* Open Close TOC */}
       <details
         className="border-[1px] border-solid border-dark dark:border-light text-dark
         dark:text-light rounded-lg p-4 sticky top-6 max-h-[80vh] overflow-hidden overflow-y-auto"
         open
       >
-
-        {/* ##### Table of Contents String ##### */}
-        <summary
-          className="text-lg font-semibold capitalize cursor-pointer"
-          style={{ fontSize: '1.25rem' }}  // Larger font size for overarching TOC summary
-        >
+        <summary className="text-lg font-semibold capitalize cursor-pointer">
           Table Of Content
         </summary>
 
-        {/* ##### List of inner ##### */}
-        <ul className="mt-4 font-in text-base">
+        {/* Add horizontal divider after the "Table Of Content" summary */}
+        <hr className="border-t border-solid border-dark/20 dark:border-light/20 my-4" />
 
+        <ul className="mt-4 font-in text-base">
           {/* Iterate over each level 2 heading */}
           {Object.entries(blog.toc).map(([headingText, { slug, subheadings }], index) => (
-
             <li key={`#${slug}`} className="mb-2">
               <details>
-
-                {/* ##### Each Heading 2 ##### */}
                 <summary
                   className="cursor-pointer hover:underline"
                 >
@@ -37,10 +28,8 @@ const BlogToc = ({ blog }) => {
                   </a>
                 </summary>
 
-                {/* ##### Heading 3 List ##### */}
                 <ul className="ml-4 mt-2">
-                  
-                  {/* ##### Iterate over each level 3 subheading under the current level 2 heading ##### */}
+                  {/* Iterate over each level 3 subheading under the current level 2 heading */}
                   {subheadings.map(({ text, slug }) => (
                     <li key={`#${slug}`} className="py-1">
                       <a
@@ -48,12 +37,9 @@ const BlogToc = ({ blog }) => {
                         data-level="three"
                         className="pl-4 sm:pl-6 flex items-center justify-start"
                       >
-                        {/* ##### Dot Thing ##### */}
-                        <span className="flex w-1 h-1 rounded-full bg-dark mr-2">
+                        <span className="flex w-1 h-1 rounded-full bg-dark dark:bg-light mr-2">
                           &nbsp;
                         </span>
-
-                        {/* ##### Heading 3 Item ##### */}
                         <span className="hover:underline">{text}</span>
                       </a>
                     </li>
@@ -61,16 +47,12 @@ const BlogToc = ({ blog }) => {
                 </ul>
               </details>
 
-              {/* ##### Horizontal divider after each level 2 heading, except the last one ##### */}
+              {/* Add horizontal divider after each level 2 heading, except the last one */}
               {index < Object.entries(blog.toc).length - 1 && (
-                <hr className="border-t border-solid border-dark/20 my-4" />
+                <hr className="border-t border-solid border-dark/20 dark:border-light/20 my-4" />
               )}
             </li>
-
-
           ))}
-
-
         </ul>
       </details>
     </div>
