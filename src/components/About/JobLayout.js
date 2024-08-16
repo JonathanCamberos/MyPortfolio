839e701a2f5865766dea2ff4193605a8b05f5127
JonathanCamberos <jonathancamberos@gmail.com>
2024-08-15 18:14:55 -0600

    about: fixed right hand formatting

--- src/components/About/JobLayout.js
+++ src/components/About/JobLayout.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const JobLayout = ({ title, company, date, description }) => {
-    return (
-      <div className="mb-6">
-        <div className="relative">
-          <div className="absolute w-3 h-3 bg-orange-500 dark:bg-yellow-400 rounded-full -left-6 top-1"></div>
+  return (
+    <div className="mb-6">
+      <div className="flex justify-between items-start">
+        {/* Job Title and Date */}
+        <div className="flex flex-col">
           <h3 className="font-semibold">{title}</h3>
           <p className="text-sm text-gray-600 dark:text-gray-300">{company}</p>
-          <p className="text-sm text-gray-600 dark:text-gray-300">{date}</p>
-          <p className="mt-2 dark:text-gray-400">{description}</p>
         </div>
+        <span className="text-accent dark:text-accentDark text-sm font-light">{date}</span>
       </div>
-    );
-  };
-  
-  export default JobLayout;
+      <p className="mt-2 dark:text-gray-400">{description}</p>
+    </div>
+  );
+};
+
+export default JobLayout;
