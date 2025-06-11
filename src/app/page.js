4083086bc046a57812d5ef2b67505fa841ece387
Jonathan Camberos <jonathancamberos@gmail.com>
2025-06-10 21:34:37 -0600

    seo-1 attempt at search engine

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,62 @@
-import { allBlogs } from "../../.contentlayer/generated"
+import { allBlogs } from "../../.contentlayer/generated";
 import FeaturedPosts from "../components/Home/FeaturedPosts";
 import HomeCoverSection from "../components/Home/HomeCoverSection";
 import RecentPosts from "../components/Home/RecentPosts";
-
-// the page.js is a ui that is unique to every route
-// page.js is directly under the app directory and represents https://localhost:3000
+import Head from "next/head";
 
 export default function Home() {
     return (
+        <>
+            {/* SEO Metadata */}
+            <Head>
+                <title>Jonathan Camberos - Software Developer Portfolio</title>
+                <meta
+                    name="description"
+                    content="Portfolio Resume of Jonathan Camberos, software development, cybersecurity, leetcode, projects."
+                />
+                <meta
+                    name="keywords"
+                    content="Jonathan Camberos, software developer, software development, portfolio, resume, cybersecurity, programming"
+                />
+                <meta name="author" content="Jonathan Camberos" />
+                <meta property="og:title" content="Jonathan Camberos - Software Developer Portfolio" />
+                <meta
+                    property="og:description"
+                    content="Portfolio Resume of Jonathan Camberos, software development, cybersecurity, leetcode, projects."
+                />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://portfolio-jonathan-camberos.vercel.app/" />
+                {/* <meta property="og:image" content="/path-to-thumbnail.jpg" /> */}
+                {/* <meta name="twitter:card" content="summary_large_image" /> */}
+                {/* <meta name="twitter:title" content="Jonathan Camberos - Software Developer Portfolio" /> */}
+                {/* <meta name="twitter:description" content="Explore the portfolio of Jonathan Camberos." /> */}
+                {/* <meta name="twitter:image" content="/path-to-thumbnail.jpg" /> */}
 
-    /* ##### Main Tag for SEO loading #####*/
-    <main className="flex flex-col items-center justify-center">
-      <HomeCoverSection blogs={allBlogs} />
-      <FeaturedPosts blogs={allBlogs} />
-      {/* <RecentPosts blogs={allBlogs}/> */}
-    </main>
-  )
+                {/* Structured Data */}
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify({
+                            "@context": "https://schema.org",
+                            "@type": "Person",
+                            "name": "Jonathan Camberos",
+                            "url": "https://portfolio-jonathan-camberos.vercel.app/",
+                            "sameAs": [
+                                "https://www.linkedin.com/in/jonathancamberos/",
+                                "https://github.com/JonathanCamberos"
+                            ],
+                            "jobTitle": "Software Developer",
+                        }),
+                    }}
+                />
+            </Head>
+
+            {/* Main Content */}
+            <main className="flex flex-col items-center justify-center">
+                <HomeCoverSection blogs={allBlogs} />
+                <FeaturedPosts blogs={allBlogs} />
+                {/* <RecentPosts blogs={allBlogs}/> */}
+            </main>
+        </>
+    );
 }
