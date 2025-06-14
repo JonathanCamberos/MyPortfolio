3b0c6a850413a26eff1ef544ad0eecb210ade3cf
Jonathan Camberos <jonathancamberos@gmail.com>
2025-06-14 10:00:14 -0600

    mainpage-2 resize icons

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -8,7 +8,7 @@ const FeaturedPosts = ({ blogs }) => {
     return <section className="w-full px-5 sm:px-10 md:px-24  sxl:px-32 flex flex-col items-center justify-center">
       <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">Currently Working On:</h2>
   
-      <div className="grid grid-cols-2 grid-rows-2 gap-6 mt-10 sm:mt-16">
+      <div className="grid grid-cols-3 grid-rows-2 gap-6 mt-10 sm:mt-16">
         <article className="relative">
           <NotesDisplayOne blog={sortedBlogs[0]} />
         </article>
@@ -16,10 +16,25 @@ const FeaturedPosts = ({ blogs }) => {
           <NotesDisplayOne blog={sortedBlogs[1]} />
         </article>
         <article className="relative">
-          <NotesDisplayOne blog={sortedBlogs[2  ]} />
+          <NotesDisplayOne blog={sortedBlogs[2]} />
         </article>
         <article className="relative">
           <NotesDisplayOne blog={sortedBlogs[3]} />
+        </article>
+        <article className="relative">
+          <NotesDisplayOne blog={sortedBlogs[4]} />
+        </article>
+        <article className="relative">
+          <NotesDisplayOne blog={sortedBlogs[5]} />
+        </article>
+        <article className="relative">
+          <NotesDisplayOne blog={sortedBlogs[6]} />
+        </article>
+        <article className="relative">
+          <NotesDisplayOne blog={sortedBlogs[7]} />
+        </article>
+        <article className="relative">
+          <NotesDisplayOne blog={sortedBlogs[8]} />
         </article>
       </div>
     </section>;
