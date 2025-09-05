094119caf8eded2e7ac321e1a26e2da753241a6a
Jonathan Camberos <jonathancamberos@gmail.com>
2025-09-04 22:44:44 -0600

    systemdesign-url-shortener

---
+++ src/app/SystemDesign/page.js
@@ -0,0 +1,42 @@
+import { allBlogs } from "../../../.contentlayer/generated";
+import GithubSlugger from "github-slugger";
+import QuestionSection from "../../components/LeetCode/QuestionSection";
+import DifficultySection from "../../components/LeetCode/Difficulty/DifficultySection";
+import ApplicationSection from "../../components/LeetCode/Application/ApplicationSection";
+import SolutionSection from "../../components/LeetCode/Solutions/SolutionSection";
+
+const slugger = new GithubSlugger();
+
+export async function generateStaticParams() {
+  const categories = [];
+  const paths = [{ slug: "all" }];
+
+  allBlogs.map((blog) => {
+    if (blog.isPublished) {
+      blog.tags.map((tag) => {
+        let slugified = slugger.slug(tag);
+        if (!categories.includes(slugified)) {
+          categories.push(slugified);
+          paths.push({ slug: slugified });
+        }
+      });
+    }
+  });
+
+  return paths;
+}
+
+const LeetCodePage = ({ params }) => {
+  return (
+    <article className="mt-20 flex flex-col text-dark dark:text-light">
+      {/* <SolutionSection />
+      <ApplicationSection />
+      <QuestionSection />
+      <DifficultySection /> */}
+
+      System design!
+    </article>
+  );
+};
+
+export default LeetCodePage;
